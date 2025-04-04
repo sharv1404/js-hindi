f3cb505bc7e87e900f949828d19c60916b5dadbe
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-04 11:45:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS codeas a newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3 
+    +
+     3)//code readability should be high
+
+//documentation TC39 and MDN, The Global Object,
+console.log("Sharvari")
+
+let name = "Sharvari"
+let age = 24
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);//output = object
+console.log(typeof undefined); //output = undefined
