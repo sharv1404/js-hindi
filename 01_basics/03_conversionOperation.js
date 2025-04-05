3327e579909bab3f6592ab8fca96c2a7a626c043
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-05 07:53:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+//let score = 33 // number
+//let score= "33" // string
+//let score= "33abc"
+//let score= null // 0=ZERO
+//let score = undefined // NaN
+//let score = true // 1
+// let score="Sharvri" // NaN
+
+// console.log(typeof score); 
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // "33abc" = NaN = Not a Number
+
+let isLoggedIn = 1 // false = 0 //string= true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // true
+
+//true => 1; false => 0
+//"" => false
+//"sharvari" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) // 33
+console.log(typeof stringNumber) // string
