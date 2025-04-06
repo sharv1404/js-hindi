23f5c4db0766295b5702415c780f919a5078745d
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-06 10:58:06 +0000

    memory summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -15,7 +15,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId); // false
+//console.log(id === anotherId); // false
 const bigNumber = 34567345678n
 
 // Reference (Non Primitive)
@@ -36,8 +36,33 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber); // undefined
-console.log(typeof outsideTemp); // object
-console.log(typeof scoreValue); // number
-console.log(typeof myFunction); // function... but it's object function
-console.log(typeof anotherId); // symbol
+// console.log(typeof bigNumber); // undefined
+// console.log(typeof outsideTemp); // object
+// console.log(typeof scoreValue); // number
+// console.log(typeof myFunction); // function... but it's object function
+// console.log(typeof anotherId); // symbol
+
+
+// +++++++++++++++++ MEMORY ++++++++++++++++++++
+
+// 1. STACK MEMORY (primitive) AND  2. HEAP MEMORY (non primitive)
+
+let myYoutube = "sharvarithigale"
+let anotherName = myYoutube
+anotherName = "chaiaurcode"
+
+console.log(myYoutube); //sharvarithigale
+console.log(anotherName); //chaiaurcode
+
+
+let userOne ={
+    email: "user@gmail.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "sharvari@gmail.com" // email changes user@gmail.com to sharvari@gmail.com
+
+console.log(userOne.email); // sharvari@gmail.com
+console.log(userTwo.email); // sharvari@gmail.com
