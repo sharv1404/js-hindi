a971029b9c0c2d148969cca6df782774a640b3e6
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-10 10:49:18 +0000

    Array function

--- 02_basics/02_array_function.js
+++ 02_basics/02_array_function.js
@@ -9,5 +9,19 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros) // concat gives new array after combining other arrays
 // console.log(allHeros);
 
-const all_new_heros = [...dc_heros, ...marvel_heros]
-console.log(all_new_heros);
+// const all_new_heros = [...dc_heros, ...marvel_heros]
+// console.log(all_new_heros);
+
+ const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+//console.log(real_another_array);
+
+console.log(Array.isArray("vampire"));
+console.log(Array.from("vampire"));
+console.log(Array.from({name: "Demon"})); // intresting case
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // [100, 200, 300]
