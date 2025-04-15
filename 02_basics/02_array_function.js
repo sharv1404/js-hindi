42637a8f458e311bd40942a8106c08b8c1d9585d
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-15 12:01:49 +0000

    array fun

--- 02_basics/02_array_function.js
+++ 02_basics/02_array_function.js
@@ -12,7 +12,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const all_new_heros = [...dc_heros, ...marvel_heros]
 // console.log(all_new_heros);
 
- const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity)
 //console.log(real_another_array);
 
