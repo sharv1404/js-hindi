043bcd9a3752c9e1be64aca3a62b3a6d6ed94dd3
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-16 12:08:10 +0000

    2nd part of objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -37,3 +37,34 @@ JsUser.greetingTwo = function(){
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+
+//const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "sharvari",
+            lastname: "thigale",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+
+// const obj3 = {
+//     obj1,
+//     obj2
+// }
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3)
