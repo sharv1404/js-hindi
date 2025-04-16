be77cae30995f70f2786f148fff97b9898664d27
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-16 14:50:37 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -61,10 +61,11 @@ const regularUser = {
 
 const obj1 = { 1: "a", 2: "b" }
 const obj2 = { 3: "a", 4: "b" }
+const obj4 = { 5: "a", 6: "b" }
 
 // const obj3 = {
 //     obj1,
 //     obj2
 // }
-const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = Object.assign({}, obj1, obj2, obj4)
 console.log(obj3)
