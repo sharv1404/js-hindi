ca4b4b16fd04a93ce74e2244c806fbb016ed79fd
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-18 05:56:01 +0000

    Objects in js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -72,4 +72,19 @@ const obj4 = { 5: "a", 6: "b" }
 // console.log(obj3)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
+
+const user = [
+    {
+        id : 1,
+        email : "user1@gmail.com",
+    },
+    {
+        id : 1,
+        email : "user1@gmail.com",
+    },
+    {
+        id : 1,
+        email : "user1@gmail.com",
+    },
+]
