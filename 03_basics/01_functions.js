401c938c8695d05f84397df3160307fa437719ab
Sharvari Thigale <162893880+sharv1404@users.noreply.github.com>
2025-04-24 10:39:48 +0000

    Functions end

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,4 +39,38 @@ function loginUserMessage(username = "Sam"){
 }
 
 // console.log(loginUserMessage("Sharvari")) // Sharvari just logged in
-console.log(loginUserMessage()) // empty space leads to ----> undefined just logged in
+//console.log(loginUserMessage()) // empty space leads to ----> undefined just logged in
+
+
+function calculateCartPrice(val1, val2, ...num1){  //rest operator and spred operator
+    return num1
+}
+
+//console.log(calculateCartPrice(300, 400, 500, 2000)); // [500,2000] because---> val1=300 , val2=400, ...num1=500,2000
+
+const user ={
+    username: "Sharvari",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(` Username is ${anyobject.username} and Price is ${anyobject.price} `);
+    
+}
+
+// handleObject(user) // Username is Sharvari and Price is 199
+handleObject({
+    username : "Sam",
+    price: 349,
+}) // direct calling function method 
+
+
+const myNewArray = [100, 50, 200, 150]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 50, 200, 150]));
+
